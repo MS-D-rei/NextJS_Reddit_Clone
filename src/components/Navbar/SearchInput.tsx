1b352d2b8069ebd8170d5982b442d9fb27f546f7
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-02-03 15:47:24 +0900

    add SearchInput to Navbar

---
+++ src/components/Navbar/SearchInput.tsx
@@ -0,0 +1,27 @@
+import { SearchIcon } from '@chakra-ui/icons';
+import { Flex, Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+
+export default function SearchInput() {
+  return (
+    <Flex flexGrow={1} mr={2} align='center'>
+      <InputGroup>
+        <InputLeftElement pointerEvents="none" height='2rem'>
+          <SearchIcon color="gray.400" />
+        </InputLeftElement>
+        <Input
+          placeholder="Search reddit"
+          fontSize="0.8rem"
+          _placeholder={{ color: 'gray.500' }}
+          _hover={{ bg: 'white', border: '1px solid', borderColor: 'blue.400' }}
+          _focus={{
+            outline: 'none',
+            border: '1px solid',
+            borderColor: 'blue.400',
+          }}
+          height='2rem'
+          bg="gray.50"
+        />
+      </InputGroup>
+    </Flex>
+  );
+}
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,12 +1,21 @@
 import { Flex, Image } from '@chakra-ui/react';
+import SearchInput from '@/components/Navbar/SearchInput';
 
 export default function Navbar() {
   return (
     <Flex bg={'white'} height="3rem" padding="0.5rem 1rem">
-      <Flex align='center'>
+      <Flex align="center">
         <Image src="images/redditFace.svg" alt="redditFace" height="2rem" />
-        <Image src="images/redditText.svg" alt="redditText" height={'3rem'} />
+        <Image
+          src="images/redditText.svg"
+          alt="redditText"
+          height={'3rem'}
+          display={{ base: 'none', md: 'unset' }}
+        />
       </Flex>
+      <SearchInput />
+      {/* <Dicrectory /> */}
+      {/* <RightContent /> */}
     </Flex>
   );
 }
