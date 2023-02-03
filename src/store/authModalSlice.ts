abe3ef616285757671fb89730bff8b62e0e5dc8b
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-02-03 19:58:21 +0900

    add closeModal actions to authModalSlice

--- src/store/authModalSlice.ts
+++ src/store/authModalSlice.ts
@@ -1,28 +1,32 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-type AuthModalViewType = 'login' | 'signup' | 'resetPassword'
+type AuthModalViewType = 'login' | 'signup' | 'resetPassword';
 
 export interface AuthModalState {
   isOpen: boolean;
-  view?: AuthModalViewType;
+  view: AuthModalViewType;
 }
 
 const initialState: AuthModalState = {
   isOpen: false,
+  view: 'login',
 };
 
 export const authModalSlice = createSlice({
   name: 'authModal',
   initialState,
   reducers: {
-    open: (state, actions: PayloadAction<AuthModalViewType>) => {
-      state.isOpen = true
-      state.view = actions.payload
+    openModal: (state, actions: PayloadAction<AuthModalViewType>) => {
+      state.isOpen = true;
+      state.view = actions.payload;
     },
+    closeModal: (state) => {
+      state.isOpen = false;
+    }
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { open } = authModalSlice.actions;
+export const { openModal, closeModal } = authModalSlice.actions;
 
 export default authModalSlice.reducer;
