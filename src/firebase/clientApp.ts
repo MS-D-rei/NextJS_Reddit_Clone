9d24967410a2690fa06f49b5cad20106ba5d5203
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-02-05 22:29:51 +0900

    add clientApp file for firebase

---
+++ src/firebase/clientApp.ts
@@ -0,0 +1,27 @@
+import { initializeApp, getApp, getApps } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXTJS_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXTJS_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXTJS_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXTJS_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXTJS_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXTJS_PUBLIC_FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase for SSR
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+export { app, firestore, auth, storage };
