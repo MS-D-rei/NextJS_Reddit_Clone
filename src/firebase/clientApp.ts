5c4241ff2e407fb008229a1258f5fd85bece1ec9
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-02-10 21:25:32 +0900

    modify exposed environment valuables setting in clientApp

--- src/firebase/clientApp.ts
+++ src/firebase/clientApp.ts
@@ -7,21 +7,27 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXTJS_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXTJS_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXTJS_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXTJS_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXTJS_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXTJS_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
+
+// console.log(firebaseConfig)
 
 // Initialize Firebase
 // const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase for SSR
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+// console.log(app);
 const firestore = getFirestore(app);
+// console.log(firestore);
 const auth = getAuth(app);
+// console.log(auth);
 const storage = getStorage(app);
+// console.log(storage);
 
 export { app, firestore, auth, storage };
