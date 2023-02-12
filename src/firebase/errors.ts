0eb6f5ace6e39ea4e0ce7636eb9ff2d0da691d33
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-02-12 17:05:07 +0900

    update firebase error patterns in errors.ts

--- src/firebase/errors.ts
+++ src/firebase/errors.ts
@@ -5,4 +5,6 @@ interface FirebaseErrors {
 export const FIREBASE_ERRORS: FirebaseErrors = {
   'Firebase: Error (auth/email-already-in-use).':
     'A user with that email already exists',
+  'Firebase: Error (auth/user-not-found).': 'User not found',
+  'Firebase: Error (auth/wrong-password).': 'Wrong password',
 };
