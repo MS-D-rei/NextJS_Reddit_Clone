71f0b4374a8acdfb122eece612c587bce32f39ec
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-02-12 15:02:32 +0900

    modify size of google icon

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -4,7 +4,7 @@ export default function OAuthButtons() {
   return (
     <Flex direction="column" mb={4}>
       <Button variant="oauth" mb={4}>
-        <Image src='/images/googlelogo.png' alt='google logo' height='1.5rem' mr={4} />
+        <Image src='/images/googlelogo.png' alt='google logo' height='1.2rem' mr={4} />
         Continue with Google
       </Button>
       <Button variant="oauth">Continue with others</Button>
