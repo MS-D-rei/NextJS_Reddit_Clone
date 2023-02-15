1f935a3cb24dc6bcf2b224ce33880be26d923a67
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-02-15 21:04:04 +0900

    add useEffect to close modal after login to Auth Modal

--- src/components/Modal/Auth/index.tsx
+++ src/components/Modal/Auth/index.tsx
@@ -14,10 +14,21 @@ import {
 } from '@chakra-ui/react';
 import AuthInputs from '@/components/Modal/Auth/AuthInputs';
 import OAuthButtons from '@/components/Modal/Auth/OAuthButtons';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase/clientApp';
+import { useEffect } from 'react';
 
 export default function AuthModal() {
   const dispatch = useAppDispatch();
   const state = useAppSelector((state) => state.authModal);
+
+  const [user, loading, authError] = useAuthState(auth);
+
+  // if current user exists, close auth modal
+  useEffect(() => {
+    if (user) dispatch(closeModal());
+  }, [dispatch, user])
+
   const modalCloseHandler = () => {
     dispatch(closeModal());
   };
