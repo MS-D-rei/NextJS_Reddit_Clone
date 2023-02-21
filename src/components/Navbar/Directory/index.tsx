054cf221a6047d6909999cfab84e6b287d4fc3a2
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-02-21 18:13:22 +0900

    add Directory component to show communities

---
+++ src/components/Navbar/Directory/index.tsx
@@ -0,0 +1,29 @@
+import { ChevronDownIcon } from '@chakra-ui/icons';
+import { Flex, Icon, Menu, MenuButton, MenuList, Text } from '@chakra-ui/react';
+import { TiHome } from 'react-icons/ti';
+
+export default function Directory() {
+  return (
+    <Menu>
+      <MenuButton
+        cursor="pointer"
+        padding="0px 0.5rem"
+        borderRadius={4}
+        _hover={{ outline: '1px solid', outlineColor: 'gray.200' }}
+        mr={2}
+        ml={{ base: 0, md: 2 }}
+      >
+        <Flex alignItems="center" justifyContent='space-between' width={{ base: 'auto', lg: '200px' }} >
+          <Icon as={TiHome} fontSize={24} mr={{ base: 1, md: 2 }} />
+          <Flex display={{ base: 'none', lg: 'flex' }}>
+            <Text fontWeight='600'>Home</Text>
+          </Flex>
+          <ChevronDownIcon />
+        </Flex>
+      </MenuButton>
+      <MenuList>
+        {/* <Communities /> */} Commnuties
+      </MenuList>
+    </Menu>
+  );
+}
