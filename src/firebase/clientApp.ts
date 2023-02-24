a6883ff2af68baf396d2b96e86fc67d32ed7d08d
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-02-24 21:18:32 +0900

    add appCheck for security to clientApp component

--- src/firebase/clientApp.ts
+++ src/firebase/clientApp.ts
@@ -2,6 +2,7 @@ import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -29,5 +30,14 @@ const auth = getAuth(app);
 // console.log(auth);
 const storage = getStorage(app);
 // console.log(storage);
+
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider(process.env.NEXT_PUBLIC_APP_CHECK),
+  // Optional argument. If true, the SDK automatically refreshes App Check tokens as needed.
+  // By default, isTokenAutoRefreshEnabled: false
+  isTokenAutoRefreshEnabled: false,
+});
 
 export { app, firestore, auth, storage };
