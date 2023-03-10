f110f1f4aef96c91123c8170f6f9ead725be3959
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-03-10 22:49:03 +0900

    add PostItem to PostList

--- src/components/Post/PostList.tsx
+++ src/components/Post/PostList.tsx
@@ -1,7 +1,11 @@
-import { useEffect} from 'react';
-import { ICommunity } from '@/store/communitySlice';
+import { useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase/clientApp';
 import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import { ICommunity } from '@/store/communitySlice';
 import { getAllPosts } from '@/store/postSlice';
+import PostItem from '@/components/Post/PostItem';
+import { Flex } from '@chakra-ui/react';
 
 interface PostListProps {
   communityData: ICommunity;
@@ -12,6 +16,8 @@ export default function PostList({ communityData }: PostListProps) {
 
   const postState = useAppSelector((state) => state.post);
 
+  const [user] = useAuthState(auth);
+
   useEffect(() => {
     dispatch(getAllPosts({ communityId: communityData.id }));
   }, []);
@@ -19,7 +25,12 @@ export default function PostList({ communityData }: PostListProps) {
   return (
     <>
       {postState.posts.map((post) => (
-        <div key={post.id}>{post.id}</div>
+        <PostItem
+          key={post.id}
+          post={post}
+          userIsCreator={user?.uid === communityData.creatorId}
+          voteValue={post.voteStatus}
+        />
       ))}
     </>
   );
