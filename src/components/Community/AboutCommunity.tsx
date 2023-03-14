b9028ca25260e5aa54e65e45197a31099b2626ce
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-03-14 15:51:30 +0900

    add community image change section to AboutCommunity component

--- src/components/Community/AboutCommunity.tsx
+++ src/components/Community/AboutCommunity.tsx
@@ -1,19 +1,30 @@
-import { ICommunity } from '@/store/communitySlice';
+import { ChangeEvent, useRef, useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { format } from 'date-fns';
+import { ja } from 'date-fns/locale';
 import {
   Box,
   Button,
   Divider,
   Flex,
   Icon,
+  Image,
+  Input,
+  Spinner,
   Stack,
   Text,
 } from '@chakra-ui/react';
-import { format } from 'date-fns';
 import { HiOutlineDotsHorizontal } from 'react-icons/hi';
 import { RiCakeLine } from 'react-icons/ri';
-import { ja } from 'date-fns/locale';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, firestore, storage } from '@/firebase/clientApp';
+import { ICommunity, updateCurrentCommunityImageURL } from '@/store/communitySlice';
+import { useSelectFile } from '@/hooks/useSelectFile';
+import { FaReddit } from 'react-icons/fa';
+import { getDownloadURL, ref, uploadString } from 'firebase/storage';
+import { doc, FirestoreError, updateDoc } from 'firebase/firestore';
+import { useAppDispatch } from '@/store/hooks';
 
 interface AboutCommunityProps {
   communityData: ICommunity;
@@ -21,6 +32,52 @@ interface AboutCommunityProps {
 
 export default function AboutCommunity({ communityData }: AboutCommunityProps) {
   const router = useRouter();
+  const selectedFileRef = useRef<HTMLInputElement>(null);
+  const [isUploadingImage, setIsUploadingImage] = useState(false);
+  const [updateImageError, setUpdateImageError] = useState('');
+
+  const dispatch = useAppDispatch();
+
+  const [user] = useAuthState(auth);
+  const { selectedFile, setSelectedFile, selectFile } = useSelectFile();
+
+  const changeImageHandler = () => {
+    selectedFileRef.current?.click();
+  };
+
+  const updateImageHandler = async () => {
+    // if no selectedFile, do nothing.
+    if (!selectedFile) return;
+
+    setIsUploadingImage(true);
+
+    try {
+      // upload the selectedFile to fire storage.
+      const imageRef = ref(storage, `communities/${communityData.id}/image`);
+      await uploadString(imageRef, selectedFile, 'data_url');
+      const downloadURL = await getDownloadURL(imageRef);
+
+      // update the community imageURL field.
+      const communityDocRef = doc(firestore, 'communities', communityData.id);
+      await updateDoc(communityDocRef, {
+        imageURL: downloadURL,
+      });
+
+      // update communityState.currentCommunity
+      dispatch(updateCurrentCommunityImageURL(downloadURL));
+    } catch (err) {
+      if (err instanceof Error) {
+        console.log(`${err.name}: ${err.message}`);
+        setUpdateImageError(`${err.name}: ${err.message}`);
+      }
+      if (err instanceof FirestoreError) {
+        console.log(`${err.name}: ${err.message}`);
+        setUpdateImageError(`${err.name}: ${err.message}`);
+      }
+    }
+
+    setIsUploadingImage(false);
+  };
 
   return (
     <Box position="sticky" top="14px">
@@ -78,11 +135,62 @@ export default function AboutCommunity({ communityData }: AboutCommunityProps) {
               </Text>
             )}
           </Flex>
+          {/* create post button */}
           <Link href={`/r/${router.query.communityId}/submit`}>
-            <Button mt={3} width='100%' height="30px">
+            <Button mt={3} width="100%" height="30px">
               Create Post
             </Button>
           </Link>
+          {/* community creator can change community image */}
+          {user?.uid === communityData.creatorId && (
+            <>
+              <Divider />
+              <Stack spacing={1} fontSize="10pt">
+                <Text fontWeight={600}>Admin</Text>
+                <Flex alignItems="center" justifyContent="space-between">
+                  <Text
+                    color="blue.500"
+                    cursor="pointer"
+                    _hover={{ textDecoration: 'underline' }}
+                    onClick={changeImageHandler}
+                  >
+                    Change Image
+                  </Text>
+                  {communityData.imageURL || selectedFile ? (
+                    <Image
+                      src={selectedFile || communityData.imageURL}
+                      borderRadius="full"
+                      boxSize="40px"
+                      alt="CommunityImage"
+                    />
+                  ) : (
+                    <Icon
+                      as={FaReddit}
+                      fontSize={40}
+                      color="brand.100"
+                      mr={2}
+                    />
+                  )}
+                </Flex>
+                {selectedFile &&
+                  (isUploadingImage ? (
+                    <Spinner />
+                  ) : (
+                    <Text cursor="pointer" onClick={updateImageHandler}>
+                      Save Changes
+                    </Text>
+                  ))}
+                <Input
+                  id="file-upload"
+                  type="file"
+                  accept="image/x-png, image/gif, image/jpeg"
+                  hidden
+                  ref={selectedFileRef}
+                  onChange={selectFile}
+                />
+              </Stack>
+            </>
+          )}
         </Stack>
       </Flex>
     </Box>
