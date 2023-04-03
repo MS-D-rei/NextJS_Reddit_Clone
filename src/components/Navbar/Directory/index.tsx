cab006315fe62e2ededa524d97aa81fc2e8bee42
MS_D_rei <46997561+MS-D-rei@users.noreply.github.com>
2023-04-03 19:17:01 +0900

    modify directory label name to show current communit name

--- src/components/Navbar/Directory/index.tsx
+++ src/components/Navbar/Directory/index.tsx
@@ -1,9 +1,13 @@
+import { useRouter } from 'next/router';
 import { ChevronDownIcon } from '@chakra-ui/icons';
 import { Flex, Icon, Menu, MenuButton, MenuList, Text } from '@chakra-ui/react';
 import { TiHome } from 'react-icons/ti';
 import Communities from '@/components/Navbar/Directory/Communites';
 
 export default function Directory() {
+  const router = useRouter();
+  const communityName = router.query.communityId;
+
   return (
     <Menu>
       <MenuButton
@@ -14,10 +18,18 @@ export default function Directory() {
         mr={2}
         ml={{ base: 0, md: 2 }}
       >
-        <Flex alignItems="center" justifyContent='space-between' width={{ base: 'auto', lg: '200px' }} >
+        <Flex
+          alignItems="center"
+          justifyContent="space-between"
+          width={{ base: 'auto', lg: '200px' }}
+        >
           <Icon as={TiHome} fontSize={24} mr={{ base: 1, md: 2 }} />
           <Flex display={{ base: 'none', lg: 'flex' }}>
-            <Text fontWeight='600'>Home</Text>
+            {communityName ? (
+              <Text fontWeight="600">{communityName}</Text>
+            ) : (
+              <Text fontWeight="600">Home</Text>
+            )}
           </Flex>
           <ChevronDownIcon />
         </Flex>
